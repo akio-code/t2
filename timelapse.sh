0130faf199e6b5f9e74d9eaca837fc9fa1ee6733
Lucas Akio <lucastreze@gmail.com>
2017-03-23 23:47:34 -0300

    Init commit

---
+++ timelapse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function timelapse2()
+{
+	echo "timelapse $1s"
+
+	# Timelase settings
+	TIME=$1
+	FRAMES=312
+
+	# Setting camera
+	gphoto2 --set-config autopoweroff=0
+	gphoto2 --set-config reviewtime=0
+	gphoto2 --set-config imageformat=9
+	gphoto2 --set-config drivemode=0
+	gphoto2 --set-config picturestyle=3
+	gphoto2 --set-config iso=0
+	gphoto2 --set-config aeb=0
+	gphoto2 --set-config meteringmode=2
+
+	# Timelapse
+	INTERVAL=$((TIME / FRAMES))
+	gphoto2 --capture-image --interval $INTERVAL --frames $FRAMES
+
+}
---
+++ timelapse_old.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+do
+	echo "Press [CTRL+C] to stop.."
+	gphoto2 --capture-image
+	sleep 19
+done
